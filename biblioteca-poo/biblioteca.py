5395c2e11bc3bb91331014343481aaed13814d26
Caua Theodoro <caua82477@gmail.com>
2026-03-02 16:22:45 -0300

    Listando Itens

--- biblioteca-poo/biblioteca.py
+++ biblioteca-poo/biblioteca.py
@@ -1,12 +1,28 @@
-class item:
-    def __init__(self, codigo, titulo):
+class itemBiblioteca:
+    def __init__(self, codigo, titulo, tema):
         self.codigo = codigo
         self.titulo = titulo
+        self.tema = tema
 
-    def emprestar(self):
-        if self.emprestado:
-            print("Item já emprestado.")
+    def cadastrar_livro(self):
+        if self.cadastrar_livro:
+            print("Livro já cadastrado.")
         else:
-            self.emprestado = False
-            print("Item emprestado com sucesso.")
+            self.cadastrar = False
+            print("Livro cadastrado com sucesso.")
+        
+    def cadastrar_revista(self):
+        if self.cadastrar_revista:
+            print("Revista já cadastrada.")
+        else:
+            self.cadastrar = False
+            print("Revista cadastrada com sucesso.")
 
+    def listar_itens(self):
+        if self.listar_itens:
+            print("Nenhum item cadastrado.")
+        else:
+            self.cadastrar = False
+            print("\n--- Lista de Itens ---")
+            for item in item:
+                print(item)
